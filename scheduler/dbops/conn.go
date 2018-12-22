66f5cc3bc37f78e899f3a90a90b8064528742d5f
CRORCR <xingzhiheyi_code@163.com>
2018-12-22 17:45:47 +0800

    db connection

--- scheduler/dbops/conn.go
+++ scheduler/dbops/conn.go
@@ -1,7 +1,28 @@
 package dbops
 
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
 /**
- * @desc    TODO
+ * @desc    数据库连接
  * @author Ipencil
  * @create 2018/12/22
  */
+var (
+	dbConn         *sql.DB
+	err            error
+	dataSourceName = "root:root@tcp(127.0.0.1:3306)/video?charset=utf8"
+)
+
+//使用init,每次当前包被调用,都会调用init函数
+func init() {
+	dbConn, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic("DB open failed")
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		panic("DB ping failed")
+	}
+}
--- scheduler/taskrunner/tasks.go
+++ scheduler/taskrunner/tasks.go
@@ -1,7 +1,12 @@
 package taskrunner
 
 /**
- * @desc    TODO
+ * @desc    延迟删除
  * @author Ipencil
  * @create 2018/12/22
  */
+
+ //从数据库读取 需要删除的数据  存入dataChan
+
+
+ //从dataChan中读取数据,执行
